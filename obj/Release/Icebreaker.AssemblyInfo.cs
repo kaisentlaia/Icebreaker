4044b3b5fff4b51ebf794ed261a8ac14b21cf186
Aur13l <ytelli@gmail.com>
2026-04-04 17:10:28 +0300

    Add modicon.png, modify csproj to include it in build, package v1.0.1

--- obj/Release/Icebreaker.AssemblyInfo.cs
+++ obj/Release/Icebreaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Icebreaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47fe95edcdf3a04dd91f43c7610c29aa57bf2c89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb768a9fe07e2b85becfcaf26f7018a935a27675")]
 [assembly: System.Reflection.AssemblyProductAttribute("Icebreaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Icebreaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
